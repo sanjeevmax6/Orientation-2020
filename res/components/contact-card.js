a2134ad3a81d85cdb6c7f124155c070872e67d76
shivamgup0001 <shivamgup00001@gmail.com>
2021-10-22 20:03:26 +0530

    Added: Contact card

---
+++ res/components/contact-card.js
@@ -0,0 +1,68 @@
+import React from 'react';
+import {Text,Card} from '@ui-kitten/components';
+import {ImageBackground, StyleSheet,View} from 'react-native';
+import { scale, verticalScale } from 'react-native-size-matters';
+import { borderRadiusLarge, fontSizeMedium, fontSizeSmall, paddingLeftMedium, paddingRightMedium } from '../utils/UIconstants';
+import { Black, White} from '../utils/colors';
+import LinearGradient from 'react-native-linear-gradient';
+
+
+const ContactCard = () => {
+  return (
+      <>
+        <Card style={styles.card}>
+            <ImageBackground source={require('../assets/contact-card.jpg')} resizeMode='cover' style={styles.image}>
+              <LinearGradient colors={['transparent',Black]} style={styles.linearGradient} locations={[0.3,1.2]}></LinearGradient>
+              <View style={styles.textContainer}>
+              <Text style={styles.textName}>Maria</Text>
+              <Text style={styles.textBody}>Designer</Text>
+              </View>
+            </ImageBackground>
+            
+        </Card>
+        </>
+  );
+};
+
+const styles=StyleSheet.create({
+  card:{
+    height:verticalScale(160),
+    width:scale(130),
+    alignItems:'center',
+    justifyContent:'center',
+    borderRadius:borderRadiusLarge,
+    borderWidth:0,
+    margin:paddingLeftMedium
+  },
+  image:{
+    height:verticalScale(160),
+    width:scale(130),
+  },
+  linearGradient: {
+    backgroundColor: "transparent",
+        position: "absolute",
+        top: 0,
+        bottom: 0,
+        left: 0,
+        right: 0
+  },
+  textContainer:{
+    height:verticalScale(40),
+    width:scale(120),
+    position:'absolute',
+    bottom:0,
+  },
+  textName:{
+    marginLeft:scale(paddingLeftMedium),
+    fontSize:fontSizeMedium,
+    color:White
+  },
+  textBody:{
+    marginTop:verticalScale(-3),
+    marginLeft:scale(paddingLeftMedium),
+    fontSize:fontSizeSmall,
+    color:White
+  },
+});
+
+export default ContactCard;
--- res/screens/contacts/index.js
+++ res/screens/contacts/index.js
@@ -1,15 +1,34 @@
 import React from 'react';
 import {Layout, Text} from '@ui-kitten/components';
-import {SafeAreaView} from 'react-native';
+import {SafeAreaView,StyleSheet,View} from 'react-native';
+import ContactCard from '../../components/contact-card';
+import { scale } from 'react-native-size-matters';
+import { paddingLeftMedium, paddingRightMedium } from '../../utils/UIconstants';
+
 
 const Contacts = () => {
   return (
     <SafeAreaView style={{flex: 1}}>
-      <Layout>
-        <Text>Contacts</Text>
+      <Layout style={{flex:1}}>
+      <Text >Contacts</Text>
+      <View style={styles.container}>
+      <ContactCard/>
+      <ContactCard/>
+      </View>
       </Layout>
     </SafeAreaView>
   );
 };
+
+const styles=StyleSheet.create({
+  container:{
+    flex:1,
+    flexDirection:'row',
+    alignItems:'center',
+    justifyContent: 'center',
+    paddingRight:scale(paddingRightMedium),
+    paddingLeft:scale(paddingLeftMedium)
+  },
+});
 
 export default Contacts;
