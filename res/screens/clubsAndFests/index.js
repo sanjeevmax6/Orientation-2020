d5c72c95d0194e257acebbe88e5695b4f407db1a
shivamgup0001 <85842255+shivamgup0001@users.noreply.github.com>
2021-10-25 10:21:36 +0530

    Added: clubs and fests cards

--- res/screens/clubsAndFests/index.js
+++ res/screens/clubsAndFests/index.js
@@ -1,15 +1,55 @@
 import React from 'react';
 import {Layout, Text} from '@ui-kitten/components';
-import {SafeAreaView} from 'react-native';
+import {SafeAreaView, View,StyleSheet} from 'react-native';
+import { ScrollView } from 'react-native-gesture-handler';
+import ClubCard from '../../components/club-card';
+import { scale } from 'react-native-size-matters';
+import { fontSizeBig, fontSizeVeryLarge, paddingMedium, paddingSmall } from '../../utils/UIConstants';
 
 const ClubsAndFests = () => {
   return (
     <SafeAreaView style={{flex: 1}}>
-      <Layout>
-        <Text>Clubs And Fests</Text>
+      <Layout style={{flex:1}}>
+        <View style={styles.container}>
+        <Text style={styles.textLabel}>Clubs :</Text>
+        <View style={styles.clubContainer}>
+          <ScrollView horizontal={true}
+          showsHorizontalScrollIndicator={false}>
+            <ClubCard/>
+            <ClubCard/>
+          </ScrollView>
+        </View>
+        <Text style={styles.textLabel}>Fests :</Text>
+        <View style={styles.clubContainer}>
+          <ScrollView horizontal={true}
+          showsHorizontalScrollIndicator={false}>
+            <ClubCard/>
+            <ClubCard/>
+          </ScrollView>
+        </View>
+        </View>
       </Layout>
     </SafeAreaView>
   );
 };
+
+const styles=StyleSheet.create({
+  clubContainer: {
+    flexDirection:'row',
+    justifyContent:'center',
+    alignItems:'center',
+  },
+  container:{
+    flex:1,
+    justifyContent:'center',
+    alignContent:'center',
+  },
+  textLabel:{
+    fontSize:fontSizeVeryLarge,
+    paddingLeft:paddingMedium,
+    paddingBottom:paddingSmall,
+    paddingTop:paddingSmall
+  },
+});
 
 export default ClubsAndFests;
